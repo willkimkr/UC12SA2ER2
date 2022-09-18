118f76d9d6b17df0726cba40f26b92deb3e70fca
willkimkr <willkimkr@outlook.com>
2022-09-18 15:16:37 -0300

    ER4 PessoaJuridica

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using CadastroPessoa.Interfaces;
 
 namespace CadastroPessoa.Classes
@@ -14,8 +15,61 @@ namespace CadastroPessoa.Classes
         {
             throw new NotImplementedException();
         }
-
+        //xx.xxx.xxx/00001-xx  ----- xxxxxxxx0001xx
         public bool ValidarCnpj(string cnpj)
+        {
+           bool retornocnpjvalido = Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)");
+           if (retornocnpjvalido == true)
+           {
+               if (cnpj.Length == 18)
+               {
+                  string substringCnpj = cnpj.Substring(11, 4);
+
+                  if (substringCnpj == "0001")
+                  {
+                      return true;
+                  }
+
+               }else if (cnpj.Length == 14)
+               {
+                  string subStringCnpj = cnpj.Substring(8, 4);
+
+                  if (subStringCnpj == "0001")
+                  {
+                      return true;
+                  }
+
+               }
+           }
+
+           return false;
+        //    if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)")
+        // ==true)
+        //    {
+
+        //    }
+
+        //    if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+        //    {
+
+        //    } 
+
+
+        // = Atribuicao
+        // == Comparacao
+        // === Comparacao exata do mesmo item
+
+
+
+
+        }
+
+        internal bool ValidarCnpj(object cnpj)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool ValidarCnpj()
         {
             throw new NotImplementedException();
         }
--- Program.cs
+++ Program.cs
@@ -1,34 +1,57 @@
 ﻿using CadastroPessoa.Classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-PessoaFisica metodosPf = new PessoaFisica();
-Endereco novoEndPf = new Endereco();
+// PessoaFisica novaPf = new PessoaFisica();
+// PessoaFisica metodosPf = new PessoaFisica();
+// Endereco novoEndPf = new Endereco();
 
-novaPf.Nome= "willian";
-novaPf.dataNasc = new DateTime(2000,01,01);
-novaPf.Cpf = "1234567890";
-novaPf.Rendimento = 3500.5f;
+// novaPf.Nome= "willian";
+// novaPf.dataNasc = new DateTime(2000,01,01);
+// novaPf.Cpf = "1234567890";
+// novaPf.Rendimento = 3500.5f;
 
-novoEndPf.logradouro = "Alameda barao de limeira";
-novoEndPf.numero = 593;
-novoEndPf.complemento = "SENAI informatica";
-novoEndPf.endComercial = true;
+// novoEndPf.logradouro = "Alameda barao de limeira";
+// novoEndPf.numero = 593;
+// novoEndPf.complemento = "SENAI informatica";
+// novoEndPf.endComercial = true;
 
-novaPf.Endereco = novoEndPf;
+// novaPf.Endereco = novoEndPf;
 
 
-Console.WriteLine(@$"
-Nome: {novaPf.Nome}
-Endereco: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
-Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
+// Console.WriteLine(@$"
+// Nome: {novaPf.Nome}
+// Endereco: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
+// Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}");
 
-");
+
 
 //Console.WriteLine(metodosPf.ValidarDataNasc(novaPf.dataNasc));
-
-
 
 //Console.WriteLine(novaPf.Nome);
 
 //Console.WriteLine($"Nome: {novaPf.Nome} Nome: {novaPf.Nome}");
 //Console.WriteLine("Nome: " + novaPf.Nome +  " Nome: " + novaPf.Nome);
+
+PessoaJuridica novaPj = new PessoaJuridica();
+PessoaJuridica metodosPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.Nome = "Nome Pj";
+novaPj.RazaoSocial = "Razao Social Pj";
+novaPj.Cnpj = "00000000000100";
+novaPj.Rendimento = 10000.5f;
+
+novoEndPj.logradouro = "Alameda barao de Limeira";
+novoEndPj.numero = 539;
+novoEndPj.complemento = "SENAI Informatica";
+novoEndPj.endComercial = true;
+
+novaPj.Endereco = novoEndPj;
+
+// Console.WriteLine(metodosPj.ValidarCnpj(novaPj.Cnpj));
+Console.WriteLine(@$"
+Nome: {novaPj.Nome}
+Razao social: {novaPj.RazaoSocial}
+CNPJ: {novaPj.Cnpj}, Valido: {metodosPj.ValidarCnpj(novaPj.Cnpj)}
+Endereco: {novaPj.Endereco.logradouro}, Nº: {novaPj.Endereco.numero}
+Complemento: {novaPj.Endereco.complemento}
+");
